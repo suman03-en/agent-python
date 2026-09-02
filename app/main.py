ef04d082936c77d3b3598fd3b253a37f7a125d80
Suman Phuyal <sfuyal59@gmail.com>
2026-09-02 18:19:26 +0545

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@ import argparse
 import os
 import sys
 
+from dotenv import load_dotenv
 from openai import OpenAI
+
+load_dotenv()
 
 API_KEY = os.getenv("OPENROUTER_API_KEY")
 BASE_URL = os.getenv("OPENROUTER_BASE_URL", default="https://openrouter.ai/api/v1")
@@ -19,8 +22,10 @@ def main():
     client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
 
     chat = client.chat.completions.create(
-        model="anthropic/claude-haiku-4.5",
+        # model="anthropic/claude-haiku-4.5",
+        model="minimax/minimax-m3:free",
         messages=[{"role": "user", "content": args.p}],
+        extra_body={"reasoning": {"enabled": True}}
     )
 
     if not chat.choices or len(chat.choices) == 0:
@@ -29,8 +34,7 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
 
-    # TODO: Uncomment the following line to pass the first stage
-    # print(chat.choices[0].message.content)
+    print(chat.choices[0].message.content)
 
 
 if __name__ == "__main__":
